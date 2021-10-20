8634b9d1d3809ecee3d34e9e71cec52c66936830
krewiszynka <biuro.norsk@gmail.com>
2021-10-20 21:46:29 +0200

    aktualizacja html css i js
    Dodane slidebar  w html css i js
    teraz juz nic nie dziala :D

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,31 @@
 window.onscroll = function() {navscroll()};
-let navbar = document.getElementById("navbar");
-let sticky = navbar.offsetTop;
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 function navscroll() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
     }
+}
+var slideindex = 1;
+showslides(slideindex);
+function plusslides(n) {
+    showslides(slideindex += n);
+}
+function currentslide(n) {
+    showslides(slideindex = n);
+}
+function showslides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("demo");
+    var captiontext = document.getElementById("caption");
+    if (n > slides.length) {slideindex = 1}
+    if (n < 1) {slideindex = slides.length}
+    for (i = 0; i < slides.length; i++) { slides[i].getElementsByClassName.display = "none";}
+    for (i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace(" active", "");}
+    slides[slidesindex-1].getElementsByClassName.display = "block";
+    dots[slideindex-1].className += " active";
+    captiontext.innerHTML = dots[slideindex-1].alt;
 }
