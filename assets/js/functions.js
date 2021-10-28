2eb8fb96f7dd7adb904a8548cc9047a3eed0922c
krewiszynka <biuro.norsk@gmail.com>
2021-10-28 13:17:26 +0200

    nav opt, added slider
    Test update /
    updated navbar (html5)
    added slider (test)

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,5 @@
 window.onscroll = function() {navscroll()};
-var navbar = document.getElementById("navbar");
+var navbar = document.getElementsByTagName("nav");
 var sticky = navbar.offsetTop;
 function navscroll() {
     if (window.pageYOffset >= sticky) {
@@ -8,24 +8,10 @@ function navscroll() {
         navbar.classList.remove("sticky");
     }
 }
-var slideindex = 1;
-showslides(slideindex);
-function plusslides(n) {
-    showslides(slideindex += n);
-}
-function currentslide(n) {
-    showslides(slideindex = n);
-}
-function showslides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("demo");
-    var captiontext = document.getElementById("caption");
-    if (n > slides.length) {slideindex = 1}
-    if (n < 1) {slideindex = slides.length}
-    for (i = 0; i < slides.length; i++) { slides[i].getElementsByClassName.display = "none";}
-    for (i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace(" active", "");}
-    slides[slidesindex-1].getElementsByClassName.display = "block";
-    dots[slideindex-1].className += " active";
-    captiontext.innerHTML = dots[slideindex-1].alt;
-}
+var splide = new Splide( '.splide', {
+    direction: 'ttb',
+    height   : '10rem',
+    wheel    : true,
+  } );
+  
+  splide.mount();
