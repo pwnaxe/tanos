d6103029aea05096af3bc544540a74fcdd9c5c50
krewiszynka <biuro.norsk@gmail.com>
2021-10-31 01:03:44 +0200

    uopdate
    do naprawy sprawdzic console
    mapa powinna sie wczytywac

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,17 +1,32 @@
-window.onscroll = function() {navscroll()};
+window.onscroll = function() {
+    navscroll()
+};
 var navbar = document.getElementsByTagName("nav");
 var sticky = navbar.offsetTop;
 function navscroll() {
-    if (window.pageYOffset >= sticky) {
+    if(window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
-    } else {
+    }
+    else {
         navbar.classList.remove("sticky");
     }
 }
-var splide = new Splide( '.splide', {
-    direction: 'ttb',
-    height   : '10rem',
-    wheel    : true,
-  } );
-  
-  splide.mount();
+// var splide = new Splide( '.splide', {
+//     direction: 'ttb',
+//     height   : '10rem',
+//     wheel    : true,
+// } );
+
+// splide.mount();
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 5, };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+window.addEventListener('DOMContentLoaded',function () {
+  //your code here
+  myMap();
+});
