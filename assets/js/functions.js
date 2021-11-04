39cda2af5b56530169e7df2eea415363b2cfdec5
krewiszynka <biuro.norsk@gmail.com>
2021-11-04 20:38:58 +0100

    update +slider +opt +fixes
    i made some updates,
    i added section gallery2,
    fixed and added better slider,
    fixes menu and browser errors,
    added map google and fixes issues.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,32 +1,22 @@
-window.onscroll = function() {
-    navscroll()
-};
-var navbar = document.getElementsByTagName("nav");
-var sticky = navbar.offsetTop;
-function navscroll() {
-    if(window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-    }
-    else {
-        navbar.classList.remove("sticky");
-    }
-}
-// var splide = new Splide( '.splide', {
-//     direction: 'ttb',
-//     height   : '10rem',
-//     wheel    : true,
-// } );
-
-// splide.mount();
-
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(51.508742, -0.120850),
-        zoom: 5, };
+        center: new google.maps.LatLng(52.357667, 21.026999),
+        zoom: 9, };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
 window.addEventListener('DOMContentLoaded',function () {
-  //your code here
   myMap();
+});
+$(document).ready(function(){
+$('.fade').slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    appendArrows: slider,
+    centerMode: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
 });
