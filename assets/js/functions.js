e2b3de42f653c05a3b9b3b5e03cd1e3c0e9ad7d0
Paweł Szynkowski <pawelszynk@gmail.com>
2021-12-30 09:56:47 +0100

    Szybki push

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -44,6 +44,14 @@ $(document).ready(function(){
   });
 });
 
+let hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+  });
+
 function burgerFunction(){
   let button = document.getElementById("dropdown-links");
   if (button.style.display === "block") {
