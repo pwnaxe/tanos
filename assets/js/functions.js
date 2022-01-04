a0a433bdf6f51bc987e7f210befd74d80664c8c6
Marcin <bigos.dev.mail@gmail.com>
2022-01-04 10:43:11 +0100

    some small changes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,32 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-window.addEventListener('scroll', navscroll);
+// document.addEventListener("DOMContentLoaded", function () {
+// window.addEventListener("scroll", navscroll);
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
 
-function navscroll() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } 
-  else {
-    navbar.classList.remove("sticky");
-  }
-}
-})
+// function navscroll() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+// });
 
-window.addEventListener('DOMContentLoaded',function () {
+// window.addEventListener("DOMContentLoaded", function () {
+// });
+
+$(document).ready(function () {
   myMap();
-});
 
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(52.357667, 21.026999),
-        zoom: 9, };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
-
-$(document).ready(function(){
-$('.fade').slick({
+  $(".fade").slick({
     dots: false,
     autoplay: true,
     infinite: true,
@@ -34,21 +27,24 @@ $('.fade').slick({
     centerMode: true,
     speed: 500,
     fade: true,
-    cssEase: 'linear'
+    cssEase: "linear",
   });
+
+  $(".single-item").slick({});
 });
 
-$(document).ready(function(){
-  $('.single-item').slick({
-    
-  });
-});
+function myMap() {
+  const mapProp = {
+    center: new google.maps.LatLng(52.357667, 21.026999),
+    zoom: 9,
+  };
+  const map = new google.maps.Map(
+    document.getElementById("googleMap"),
+    mapProp
+  );
+}
 
-function burgerFunction(){
-  let button = document.getElementById("dropdown-links");
-  if (button.style.display === "block") {
-    button.style.display = "none";
-  } else {
-    button.style.display = "block";
-  }
+function burgerFunction() {
+  const button = document.getElementById("dropdown-links");
+  button.classList.toggle("hidden");
 }
